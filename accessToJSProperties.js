a2f874c176776b21d6e785871a49bf0cfb646f98
Aleksandra Zuchewicz <aleksandra.zuchewicz@the-eic.com>
2019-10-02 09:38:26 +0100

    Using bracket notation to access JS properties

---
+++ accessToJSProperties.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let cat = {
+  name: "Puffy",
+  color: "White"
+};
+cat["Eye Color"] = "Green";
+console.log(cat.color); // or
+console.log(cat["Eye Color"]);
+
+// use when you want create an object out of value because it will be enter by user
